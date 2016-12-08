fae364b0398825010f256bb4bc197f2fb53f7e35
tiqwab <tiqwab.ch90@gmail.com>
2016-12-08 19:12:33 +0900

    Add keywords and definition of tokens

--- compiler/getsource.py
+++ compiler/getsource.py
@@ -1,4 +1,68 @@
+from enum import Enum, unique
+
 MAXLINE = 120
+
+@unique # Assert that values of enum are different from each other
+class KeyWd(Enum):
+    Begin   = "begin"
+    End     = "end"
+    If      = "if"
+    Then    = "then"
+    While   = "while"
+    Do      = "do"
+    Ret     = "return"
+    Func    = "function"
+    Var     = "var"
+    Const   = "const"
+    Odd     = "odd"
+    Write   = "write"
+    WriteLn = "writeln"
+
+@unique
+class KeySym(Enum):
+    Plus      = "+"
+    Minus     = "-"
+    Mult      = "*"
+    Div       = "/"
+    LParen    = "("
+    RParen    = ")"
+    Equal     = "="
+    Lss       = "<"
+    Gtr       = ">"
+    NotEq     = "<>"
+    LssEq     = "<="
+    GtrEq     = ">="
+    Comma     = ","
+    Period    = "."
+    SemiColon = ";"
+    Assign    = ":="
+
+@unique
+class KeyToken(Enum):
+    Id  = "id"
+    Num = "num"
+    Nul = "nul"
+
+@unique
+class KeyEtc(Enum):
+    Letter = "letter"
+    Digit  = "digit"
+    Colon  = "colon"
+    Others = "others"
+
+class KeyTable:
+    @classmethod
+    def is_keywd(self, token):
+        return token in [x.value for x in KeyWd.__members__.values()]
+
+    @classmethod
+    def is_keysym(self, token):
+        return token in [x.value for x in KeySym.__members__.values()]
+
+class Token:
+    def __init__(self, kind, value):
+        self.kind = kind
+        self.value = value
 
 class SourceReader:
     def __init__(self, input_file):
